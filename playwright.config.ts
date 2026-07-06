95de0516b3f1ecdca3bd61aa835947a29ce712c7
DitpiPatil <diptipatil2014@gmail.com>
2026-07-06 22:17:24 +0530

    Mofified

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? '50%' : undefined,
 
  
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -41,7 +41,7 @@ export default defineConfig({
     baseURL: process.env.BASE_URL,
     screenshot: 'only-on-failure',
     video:'retry-with-video',
-    headless:false,
+    headless:!process.env.CI ? false:true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     
