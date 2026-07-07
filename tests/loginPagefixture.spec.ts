c2889f9d5cce3f483e4eab9674c425b7ba9b7c28
DitpiPatil <diptipatil2014@gmail.com>
2026-07-07 11:06:06 +0530

    Update

--- tests/loginPagefixture.spec.ts
+++ tests/loginPagefixture.spec.ts
@@ -23,7 +23,7 @@ test("user is able to Login test", async ({ loginPage, page }) => {
     await page.pause();
 })
 
-let loginTestData = ExcelHelper.readExcel("Data/LoginexcelData.xlsx", "Sheet1")
+let loginTestData = ExcelHelper.readExcel("./Data/LoginexcelData.xlsx", "Sheet1")
 for (let row of loginTestData) {
     test(`invalid login test with excel data ${row.Username}- ${row.Password}`, async ({ loginPage, page }) => {
         await loginPage.doLogin(row.Username, row.Password);
