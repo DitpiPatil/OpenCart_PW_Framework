e7a0480d9be0d0dfa17e02a475898fa38d7c343a
DitpiPatil <diptipatil2014@gmail.com>
2026-07-09 20:35:26 +0530

    update env

--- tests/loginPagefixture.spec.ts
+++ tests/loginPagefixture.spec.ts
@@ -19,7 +19,7 @@ test("forgot password link exist test", async ({ loginPage }) => {
 })
 
 test("user is able to Login test", async ({ loginPage, page }) => {
-    await loginPage.doLogin(process.env.appUSERNAME!, process.env.PASSWORD!);
+    await loginPage.doLogin(process.env.APPUSERNAME!, process.env.PASSWORD!);
     await page.pause();
 })
 
